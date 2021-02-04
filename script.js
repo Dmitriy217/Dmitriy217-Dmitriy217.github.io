82b96491ed84b1a9a67775aa10b31318a36f447f
Dmitriy217 <64257802+Dmitriy217@users.noreply.github.com>
2021-02-04 20:59:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+
+	
+	const burg = document.querySelector(".burger");
+	const menu = document.querySelector(".nav");
+
+
+	function click(){
+		menu.classList.toggle("active");
+	}
+
+
+	burg.onclick = click;
+
+};

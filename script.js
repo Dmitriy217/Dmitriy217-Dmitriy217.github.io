9c0034aa8e0667b0fad1a6c798af4d714ddf54a1
Dmitriy217 <64257802+Dmitriy217@users.noreply.github.com>
2021-02-05 22:36:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -4,13 +4,32 @@ window.onload = function () {
 	const burg = document.querySelector(".burger");
 	const menu = document.querySelector(".nav");
 
-
 	function click(){
 		menu.classList.toggle("active");
-                burg.classList.toggle("active");
+		burg.classList.toggle("active");
 	}
 
-
 	burg.onclick = click;
+
+	// -------------------------------------
+
+	const slider = document.querySelector(".slider_bg");
+
+	let act_slider = 1;
+
+	setInterval(function(){
+		slider.classList.remove("slide" + act_slider);
+		if(act_slider < 4){
+			act_slider++;
+			slider.classList.add("slide" + act_slider);
+		}
+		else{
+			act_slider = 1;
+			slider.classList.add("slide" + act_slider);
+		}
+	}, 5000);
+
+
+
 
 };
